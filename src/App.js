f3450b3915b3d5a8e1f0f51e2478606696db2543
kblizzy22 <kbailey322@gmail.com>
2024-12-12 18:14:55 -0500

    added basic search

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World
-      </header>
+      <div className="container">
+        <header className="App-header">
+          Hello World
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Kelly Bailey-Alme</footer>
+      </div>
     </div>
   );
 }
