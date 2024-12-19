c0a5b76e814207b50e2f8415f265faf770e4ce5f
kblizzy22 <kbailey322@gmail.com>
2024-12-19 17:30:26 -0500

    added phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Phonetic(props) {
+    return (
+        <div className="Phonetic">{props.phonetic}</div>
+    );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic.js";
 
 export default function Results(props) {
     if (props.results) {
         return (
             <div className="results">
                 <h2>{props.results.word}</h2>
+                <Phonetic phonetic={props.results.phonetic} />                
                 {props.results.meanings.map(function (meaning, index) {
                     return (
                         <div key={index}>
